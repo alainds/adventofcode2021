d94db3cef25473d11c7e631e3cc016e181479da8
alainds <alainds@>
2021-12-03 10:14:55 +0100

    raffinement

--- src/days/jour3.js
+++ src/days/jour3.js
@@ -3,7 +3,7 @@ import { getNbOccurrence } from "util/array"
 
 const dataBrut = input.split("\n").map((a) => a.split(""))
 
-const transformData = (d) => {
+const transposeArraysOfArrays = (d) => {
   let data = []
   d.map((a, i) =>
     a.map((b, j) => {
@@ -24,8 +24,8 @@ const calcul = (arr, fonctionRecherche = getMaxOccurrence) => {
   let resultat = [...arr]
   let position = 0
   while (resultat.length > 1) {
-    const data1 = transformData(resultat)
-    const recherche = fonctionRecherche(data1[position])
+    const arrTranspose = transposeArraysOfArrays(resultat)
+    const recherche = fonctionRecherche(arrTranspose[position])
     // eslint-disable-next-line no-loop-func
     resultat = resultat.filter((a) => a[position] === recherche)
     position++
@@ -33,12 +33,10 @@ const calcul = (arr, fonctionRecherche = getMaxOccurrence) => {
   return resultat[0].join("")
 }
 function result1() {
-  let data = transformData(dataBrut)
-  const gammaString = data.map((a) => getMaxOccurrence(a)).join("")
-  const epsilonString = data.map((a) => getMinOccurrence(a)).join("")
-  const gamma = parseInt(gammaString, 2)
-  const epsilon = parseInt(epsilonString, 2)
-  return gamma * epsilon
+  let data = transposeArraysOfArrays(dataBrut)
+  const gamma = data.map((a) => getMaxOccurrence(a)).join("")
+  const epsilon = data.map((a) => getMinOccurrence(a)).join("")
+  return parseInt(gamma, 2) * parseInt(epsilon, 2)
 }
 function result2() {
   let oxygen = calcul(dataBrut)
